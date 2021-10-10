8b78f9f117fb4a841616b32cfeeacf4395354ff4
Rolf Ebert <rolf.ebert.gcc@gmx.de>
2021-10-10 12:19:39 +0200

    first attempt to make i2c available

---
+++ esp32/esp32/esp32-i2c.ads
@@ -0,0 +1,57 @@
+        
+--  This package tries to provide the same interface as HAL.I2C from
+--  the Ada_Drivers_Library as much as possible.  This way we can
+--  leverage the drivers from that library.
+package ESP32.I2C is
+
+   type I2C_Status is
+     (Ok,
+      Err_Error,
+      Err_Timeout,
+      Busy);
+
+   subtype I2C_Data is UInt8_Array;
+
+   type I2C_Memory_Address_Size is
+     (Memory_Size_8b,
+      Memory_Size_16b);
+
+   subtype I2C_Address is UInt10;
+
+   type I2C_Port is limited interface;
+
+   type Any_I2C_Port is access all I2C_Port'Class;
+
+   procedure Master_Transmit
+     (This    : in out I2C_Port;
+      Addr    : I2C_Address;
+      Data    : I2C_Data;
+      Status  : out I2C_Status;
+      Timeout : Natural := 1000);
+
+   procedure Master_Receive
+     (This    : in out I2C_Port;
+      Addr    : I2C_Address;
+      Data    : out I2C_Data;
+      Status  : out I2C_Status;
+      Timeout : Natural := 1000);
+
+   procedure Mem_Write
+     (This          : in out I2C_Port;
+      Addr          : I2C_Address;
+      Mem_Addr      : UInt16;
+      Mem_Addr_Size : I2C_Memory_Address_Size;
+      Data          : I2C_Data;
+      Status        : out I2C_Status;
+      Timeout       : Natural := 1000);
+
+   procedure Mem_Read
+     (This          : in out I2C_Port;
+      Addr          : I2C_Address;
+      Mem_Addr      : UInt16;
+      Mem_Addr_Size : I2C_Memory_Address_Size;
+      Data          : out I2C_Data;
+      Status        : out I2C_Status;
+      Timeout       : Natural := 1000);
+
+end ESP32.I2C;
